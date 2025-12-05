81e24c60ad2a2b6ff7c97ddcd5eee97fd2ca127a
rick malik <fcinstaller@gmail.com>
2025-12-05 11:19:29 -0800

    Add files via upload

---
+++ Linux/src/make_coffee.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <stdlib.h>
+using namespace std;
+using std::locale; //::global(std::locale(""));
+int main()
+{
+int i = 10;
+cout << "Set timer (seconds).";
+cin >> i;
+string s = "x";
+cout << "Making coffee. Please standby.\n";
+sleep(i);
+system("aplay -q /home/rick/Music/Sounds/tada.wav");
+/*cout << "Cream and sugar?";//add 15 seconds
+cin >> s;
+if(s=="y")
+    {
+    cout << "okay\n";
+    sleep(3);
+    cout << "Pouring cream\n";
+    sleep(3);
+    cout << "Scooping sugar\n";
+    sleep(3);
+    cout << "clink clink clink clink clink\n";
+    sleep(6);
+    }
+    else
+    {
+    cout << "black then.\n";
+    }*/
+std::cout << "Done.\n";
+
+return 0;
+}
